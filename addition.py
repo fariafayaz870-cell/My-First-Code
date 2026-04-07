1d518d3727c656ccdb634d4d095a82f8558a6785
fariafayaz870-cell <fariafayaz870@gmail.com>
2026-04-07 21:58:41 +0530

    Practice Codes

---
+++ addition.py
@@ -0,0 +1,65 @@
+(</>)Python
+#Addition
+a=2
+b=4
+print(a+b)
+#subtraction
+a=6
+b=5
+print(a-b)
+#division
+a=8
+b=2
+print(a/b)
+#multiplication
+a=7
+b=4
+print(a*b)
+#area of rectangle
+length=20
+breadth=4
+area=(length*breadth)
+print(area)
+#height of a person
+name="fiza"
+height=170
+print(name)
+print(height)
+#brand and colour of bag
+brand="puma"
+colour="black"
+print=(brand,colour)
+#salary of a person
+name="raj"
+month="april"
+salary=20000
+print(name,month,salary)
+#simple intrest
+principal=1000
+rate=5
+time=2
+si=principal*rate*time
+print(si)
+#circumfrence of a circle
+radius=20
+circumfrence=2*3.14*radius
+print(radius,circumfrence)
+#equal to
+a=5
+b=5
+print(a==b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
