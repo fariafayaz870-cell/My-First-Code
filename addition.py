43f3eb57d3528dec96f9c6a16a9072a6ee0a90fd
fariafayaz870-cell <fariafayaz870@gmail.com>
2026-04-27 22:10:57 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -107,7 +107,44 @@ del number[0]
 print("after number[0]",number)
 number.clear()
 print("cleared list",number)
+#-ive numbers
+num= int(input("enter a number:"))
+factorial =1
+if num < 0:
+  print("factorial is does not exist for negative numbera")
+elif num == 0:
+  print("factorial is 1")
+else:
+  for i in range (1,num +1):
+    factorial = factorial*i
+    print("factorial of", num,"is", factorial)
+    #sum
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+sum=a+b
+print(sum)
+#def
+print("hello python")
+#test2()
 
+def  test2():
+    a=int(input("enter the first number"))
+    b=int(input("enter the 2nd number"))
+    sum=a+b
+    print(sum)
+
+test2()
+
+
+
+
+
+
+
+
+
+
+         
 
 
 
